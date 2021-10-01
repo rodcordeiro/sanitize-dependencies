da6d1034989cdee2e824ce73734b47da9f234574
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2021-10-01 10:31:51 -0300

    Structured the getFiles function

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,5 @@
-import { readdirSync } from "fs";
+import getFiles from "./utils/GetFiles";
 
-interface iData {
-  name: string;
-  type: "dir" | "file";
-  path: string;
-  children?: Array<iData>;
-}
-
-function getFiles(path: string) {
-  const data = readdirSync(path, {
-    withFileTypes: true,
-  });
-  const files = data.map((item) => {
-    const itempath = path + "\\" + item.name;
-    let data: iData = {
-      name: item.name,
-      type: item.isDirectory() ? "dir" : "file",
-      path: itempath,
-    };
-    if (data.type !== "dir") return data;
-    const children = getFiles(itempath);
-    data["children"] = children;
-    return data;
-  });
-  return files;
-}
-
-const files = getFiles(__dirname);
+const files = getFiles("./");
 console.log({ files });
+console.log(files[0]);
---
+++ src/utils/GetFiles.ts
@@ -0,0 +1,58 @@
+import { readdirSync } from "fs";
+
+/**
+ * Function getFiles
+ * Retrieves file tree of the given path recursively, includin children files and folders.
+ *
+ * @param path The path to start gathering the information.
+ * @param exclude A file or an array of filenames to exclude from the verification
+ *
+ * @returns a Object containing the name, type ('dir' | 'file'), path and childrens.
+ */
+export default function getFiles(
+  path: string,
+  exclude?: string | Array<string>
+) {
+  interface iData {
+    name: string;
+    type: "dir" | "file";
+    path: string;
+    children?: Array<iData | undefined>;
+  }
+  const items = readdirSync(path, {
+    withFileTypes: true,
+  });
+
+  const files = items
+    .filter((item) => {
+      const ignoredFiles = [
+        ".git",
+        "node_modules",
+        ".gitignore",
+        "yarn.lock",
+        "package.json",
+      ];
+      if (exclude && typeof exclude == "string") {
+        ignoredFiles.push(exclude);
+      } else if (exclude && Array.isArray(exclude)) {
+        exclude.forEach((item) => ignoredFiles.push(item));
+      }
+
+      if (ignoredFiles.indexOf(item.name) === -1) {
+        return item;
+      }
+    })
+    .map((item) => {
+      const itempath = path + "\\" + item.name;
+      let data: iData = {
+        name: item.name,
+        type: item.isDirectory() ? "dir" : "file",
+        path: itempath,
+      };
+      if (data.type !== "dir") return data;
+      const children = getFiles(itempath, exclude);
+      data.children = children;
+      return data;
+    });
+  return files;
+}
